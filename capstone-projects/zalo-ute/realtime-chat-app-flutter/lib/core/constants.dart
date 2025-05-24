bdf38f1bf4208b81473c00acde57a3454b3ea799
Kiến Đức Trọng <kienductrong@gmail.com>
2025-05-24 15:55:26 +0700

    config flutter

--- capstone-projects/zalo-ute/realtime-chat-app-flutter/lib/core/constants.dart
+++ capstone-projects/zalo-ute/realtime-chat-app-flutter/lib/core/constants.dart
@@ -1,7 +1,5 @@
 class Constants {
-  // static const String remoteUrl =
-  //     'https://realtime-chat-app-api-1.onrender.com';
-  static const String remoteUrl = 'http://localhost:7979';
+  static const String remoteUrl = 'https://zalo-ute-api.onrender.com';
   static const String emailPattern =
       r'^(?!.*[.]{2,})[a-zA-Z0-9.%]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$';
   static const String VERIFY_PUBLIC_POSTS = "verify_public_posts";
