7f3919792bccd6a51a6741b36685ff4305c50947
Kiến Đức Trọng <kienductrong@gmail.com>
2025-09-07 09:23:16 +0700

    mui typography

---
+++ learning-demos/react-mui/react-mui-demo/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { MuiTypography } from "./components/MuiTypography";
+
+function App() {
+  return (
+    <div className="App">
+      <MuiTypography />
+    </div>
+  );
+}
+
+export default App;
---
+++ learning-demos/react-mui/react-mui-demo/src/components/MuiTypography.tsx
@@ -0,0 +1,37 @@
+import { Typography } from "@mui/material";
+
+export const MuiTypography = () => {
+  return (
+    <div>
+      <Typography variant="h1">h1 heading</Typography>
+      <Typography variant="h2">h2 heading</Typography>
+      <Typography variant="h3">h3 heading</Typography>
+      <Typography variant="h4" gutterBottom>
+        h4 heading
+      </Typography>
+      <Typography variant="h5">h5 heading</Typography>
+
+      <Typography variant="subtitle1">Subtitle 1</Typography>
+      <Typography variant="subtitle2">Subtitle 2</Typography>
+
+      <Typography variant="body1">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </Typography>
+      <Typography variant="body2">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </Typography>
+    </div>
+  );
+};
