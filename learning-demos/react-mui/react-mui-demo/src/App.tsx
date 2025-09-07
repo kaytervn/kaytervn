e69154af720c878087c976a035cadb8c9c60332a
Kiến Đức Trọng <kienductrong@gmail.com>
2025-09-07 09:57:16 +0700

    button

--- learning-demos/react-mui/react-mui-demo/src/App.tsx
+++ learning-demos/react-mui/react-mui-demo/src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import { MuiButton } from "./components/MuiButton";
 import { MuiTypography } from "./components/MuiTypography";
 
 function App() {
   return (
     <div className="App">
-      <MuiTypography />
+      {/* <MuiTypography /> */}
+      <MuiButton />
     </div>
   );
 }
